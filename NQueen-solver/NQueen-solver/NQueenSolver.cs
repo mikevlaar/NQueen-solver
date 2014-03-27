6f3b4f489608f2fbe7c9ef046dc0923cab7db2af
micheldebeijer <nicheldebeijer89@hotmail.com>
2014-03-27 13:52:56 +0100

    die shizzle werkt!

---
+++ NQueen-solver/NQueen-solver/NQueenSolver.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NQueen_solver
+{
+    class NQueenSolver
+    {
+        int N;
+        int numberOfSolutions = 0;
+
+        public NQueenSolver(int numberOfQueens)
+        {
+            N = numberOfQueens;
+        }
+
+        /**
+         * Print the number of solutions found. 
+         */
+        public void printNumberOfSolutions()
+        {
+            Console.WriteLine(numberOfSolutions);
+        }
+
+        /**
+        * Check if queen is allowed on position.
+        * @param bool[,] board The current board to check a position on.
+        * @param int x The current row to check if a queen can be placed.
+        * @param int y The current column to check if a queen can be placed.
+        */
+        public bool Allowed(bool[,] board, int x, int y)
+        {
+            for (int i = 0; i <= x; i++)
+            {
+                if (board[i, y] || (i <= y && board[x - i, y - i]) || (y + i < N && board[x - i, y + i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /**
+         * Finds a solution for the n-queen problem. 
+         * Recursive method that prints all the solutions.
+         * @param bool[,] board The current board to find a solution for.
+         * @param int x The current row to check if a queen can be placed.
+         */
+        public void FindSolution(bool[,] board, int x)
+        {
+            for (int y = 0; y < N; y++)
+            {
+                if (x == N)
+                {
+                    numberOfSolutions++;
+                    printSolution(board);
+                    return;
+                }
+                if (Allowed(board, x, y))
+                {
+                    board[x, y] = true;
+                    FindSolution(board, x + 1);
+                    board[x, y] = false;
+                }
+            }
+        }
+
+        /**
+         * Method to print a solution.
+         * @param bool[,] board the board that needs to be printed.
+         */
+        public void printSolution(bool[,] board)
+        {
+            Console.WriteLine("solution number: {0}", numberOfSolutions); 
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(board[i, j] ? "|Q" : "| ");
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- NQueen-solver/NQueen-solver/Program.cs
+++ NQueen-solver/NQueen-solver/Program.cs
@@ -8,143 +8,17 @@ namespace NQueen_solver
 {
     class Program
     {
-        const int N = 8;
         static void Main(string[] args)
         {
-            int count = 0;
-            int[,] board = new int[N, N];
+            int N = 9;
+            bool[,] board = new bool[N, N];
 
-            //Initialize the board array to 0
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    board[i, j] = 0;
-                }
-            }
+            NQueenSolver solver = new NQueenSolver(N);
 
-            //Initialize the pointer array
-            int[] pointer = new int[N];
-            for (int i = 0; i < N; i++)
-            {
-                pointer[i] = -1;
-            }
+            solver.FindSolution(board, 0);
+            solver.printNumberOfSolutions();
 
-            //Implementation of Back Tracking Algorithm
-            for (int j = 0; ; )
-            {
-                pointer[j]++;
-                //Reset and move one column back 
-                if (pointer[j] == N)
-                {
-                    board[pointer[j] - 1, j] = 0;
-                    pointer[j] = -1;
-                    j--;
-                    if (j == -1)
-                    {
-                        Console.WriteLine("All possible configurations have been examined...");
-                        Console.ReadLine();
-                        break;
-                    }
-                }
-                else
-                {
-                    board[pointer[j], j] = 1;
-                    if (pointer[j] != 0)
-                    {
-                        board[pointer[j] - 1, j] = 0;
-                    }
-                    if (SolutionCheck(board))
-                    {
-                        j++;//move to next column
-                        if (j == N)
-                        {
-                            j--;
-                            count++;
-                            Console.WriteLine("Solution" + count.ToString() + ":");
-                            for (int p = 0; p < N; p++)
-                            {
-                                for (int q = 0; q < N; q++)
-                                {
-                                    Console.Write(board[p, q] + " ");
-                                }
-                                Console.WriteLine();
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        public static bool SolutionCheck(int[,] board)
-        {
-            //Row check
-            for (int i = 0; i < N; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < N; j++)
-                {
-                    sum = sum + board[i, j];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            //Main diagonal check
-            //above
-            for (int i = 0, j = N - 2; j >= 0; j--)
-            {
-                int sum = 0;
-                for (int p = i, q = j; q < N; p++, q++)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            //below
-            for (int i = 1, j = 0; i < N - 1; i++)
-            {
-                int sum = 0;
-                for (int p = i, q = j; p < N; p++, q++)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            //Minor diagonal check
-            //above
-            for (int i = 0, j = 1; j < N; j++)
-            {
-                int sum = 0;
-                for (int p = i, q = j; q >= 0; p++, q--)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            //below
-            for (int i = 1, j = N - 1; i < N - 1; i++)
-            {
-                int sum = 0;
-                for (int p = i, q = j; p < N; p++, q--)
-                {
-                    sum = sum + board[p, q];
-                }
-                if (sum > 1)
-                {
-                    return false;
-                }
-            }
-            return true;
+            Console.ReadLine();
         }
     }
 }
